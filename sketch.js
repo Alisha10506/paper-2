429a19077e150cd56bb2bcdafd5fed733ad2353b
Alisha10506 <72923515+Alisha10506@users.noreply.github.com>
2020-11-18 19:14:02 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,51 @@
+const Engine = Matter.Engine;
+	const World = Matter.World;
+	const Bodies = Matter.Bodies;
+	const Body = Matter.Body;
+
+	function preload()
+	{
+		
+	}
+
+	function setup() {
+		createCanvas(1200, 400);
+
+
+		engine = Engine.create();
+		world = engine.world;
+
+		//Create the Bodies Here.
+
+
+		Engine.run(engine);
+	paper= new paper(100,200,20)
+	ground= new ground(600,height,1570,20)
+	wall1= new dustbin(700,290)
+	wall2= new dustbin(900,290)
+	wall3= new bd (800,380)
+	}
+
+
+	function draw() {
+	rectMode(CENTER);
+	background("Green");
+	paper.display();
+	ground.display();
+	wall1.display();
+	wall2.display();
+	wall3.display();
+	drawSprites();
+	
+	}
+	function keyPressed(){
+	if(keyCode===UP_ARROW){
+		Matter.Body.applyForce(paper.body,paper.body.position,{x:19,y:-19})
+		
+		}
+		
+		}
+
+
+
+
